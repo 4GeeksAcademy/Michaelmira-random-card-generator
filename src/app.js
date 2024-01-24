6bd0af23add4a0a1783ce2273997bfd9a0ff5c59
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-24 06:01:26 +0000

    wrap all work into function called generateCard, create suiteIndex to call a random suit according to array length to console. this was a success

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function generateCard() {}
+
   const suite = ["hearts", "spade", "diamond", "clubs"];
   const card = [
     "2",
@@ -23,8 +24,14 @@ window.onload = function() {
     "king",
     "ace"
   ];
+  const suiteIndex = Math.floor(Math.random() * suite.length);
+
+  return console.log(suite[suiteIndex]);
 };
 
 // 1. make 2 arrays one with card suites and one identifying cards
 // 2. write a code function using math random to randomly pick a suit. 4 options.
-// 3. Link code
+// 3. write a code function using math random to randomly pick a card. 14 options.
+// 4. write another function to input results into a sample function. return suitA cardB suitA.
+// 5. transfer results to html using id.
+// 6. upload suite icons
