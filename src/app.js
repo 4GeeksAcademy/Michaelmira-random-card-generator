93a6e3cd3640972e9a1dcec8be5971edaceea2ce
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-24 06:12:26 +0000

    duplicated mathfloor random function to and created card index. returned both in single console log Test was sucessful

--- src/app.js
+++ src/app.js
@@ -25,13 +25,15 @@ window.onload = function() {
     "ace"
   ];
   const suiteIndex = Math.floor(Math.random() * suite.length);
+  const cardIndex = Math.floor(Math.random() * card.length);
 
-  return console.log(suite[suiteIndex]);
+  return console.log([suite[suiteIndex]], [card[cardIndex]]);
 };
 
-// 1. make 2 arrays one with card suites and one identifying cards
-// 2. write a code function using math random to randomly pick a suit. 4 options.
-// 3. write a code function using math random to randomly pick a card. 14 options.
-// 4. write another function to input results into a sample function. return suitA cardB suitA.
+// 1. make 2 arrays one with card suites and one identifying cards Complete
+// 2. write a code function using math random to randomly pick a suit. 4 options. Complete
+// 2b. test and return results to console. Complete
+// 3. write a code function using math random to randomly pick a card. 14 options. Complete
+// 4. write another function to input results into a sample function. return suitA cardB suitA. Complete
 // 5. transfer results to html using id.
 // 6. upload suite icons
