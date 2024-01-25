14f7d28ad9b65013434f3f2e3a6ec68a8073e60b
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-25 00:01:58 +0000

    Button created, linked to generate card completed and successful. added syntax to update card color back to black before updating content

--- src/app.js
+++ src/app.js
@@ -6,39 +6,51 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const suite = ["♥", "♠", "♦", "♣"];
-  const card = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A"
-  ];
-  const suiteIndex = Math.floor(Math.random() * suite.length);
-  const cardIndex = Math.floor(Math.random() * card.length);
+  function generateCard() {
+    const suite = ["♥", "♠", "♦", "♣"];
+    const card = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
+    const suiteIndex = Math.floor(Math.random() * suite.length);
+    const cardIndex = Math.floor(Math.random() * card.length);
 
-  const suitElement = document.getElementById("suit");
-  const suitTwoElement = document.getElementById("suitTwo");
+    const suitElement = document.getElementById("suit");
+    const suitTwoElement = document.getElementById("suitTwo");
 
-  suitElement.textContent = suite[suiteIndex];
-  suitTwoElement.textContent = suite[suiteIndex];
+    suitElement.style.color = "black";
+    suitTwoElement.style.color = "black";
 
-  const cardRankingElement = document.getElementById("cardRanking");
-  cardRankingElement.textContent = card[cardIndex];
+    suitElement.textContent = suite[suiteIndex];
+    suitTwoElement.textContent = suite[suiteIndex];
 
-  return console.log(
-    [suite[suiteIndex]],
-    [card[cardIndex]],
-    [suite[suiteIndex]]
-  );
+    const cardRankingElement = document.getElementById("cardRanking");
+    cardRankingElement.textContent = card[cardIndex];
+
+    if (suite[suiteIndex] === "♥" || suite[suiteIndex] === "♦") {
+      suitElement.style.color = "red";
+      suitTwoElement.style.color = "red";
+    }
+    return console.log(
+      [suite[suiteIndex]],
+      [card[cardIndex]],
+      [suite[suiteIndex]]
+    );
+  }
+  generateCard();
+  const generateButton = document.getElementById("generateButton");
+  generateButton.addEventListener("click", generateCard);
 };
 
 // 1. make 2 arrays one with card suites and one identifying cards Complete
@@ -48,5 +60,5 @@ window.onload = function() {
 // 4. write another function to input results into a sample function. return suitA cardB suitA. Complete
 // 5. transfer results to html using id. Complete
 // 6. upload suite icons Complete
-// 6b. Make the Heart and Diamond show up as red BEFORE
+// 6b. Make the Heart and Diamond show up as red BEFORE printing Complete
 // 7. create button to continue randomizing more cards instead of refreshing page
