a889ebfe767a4862284fce5832f0a19181c01ac5
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-25 00:33:49 +0000

    created button for draw 5 poker, created new variable array called allCards to hold every possible card generated. console.log all cards. Success.

--- src/app.js
+++ src/app.js
@@ -6,54 +6,63 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  const suits = ["♥", "♠", "♦", "♣"];
+  const cards = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
   function generateCard() {
-    const suite = ["♥", "♠", "♦", "♣"];
-    const card = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-      "A"
-    ];
-    const suiteIndex = Math.floor(Math.random() * suite.length);
-    const cardIndex = Math.floor(Math.random() * card.length);
+    const suitsIndex = Math.floor(Math.random() * suits.length);
+    const cardsIndex = Math.floor(Math.random() * cards.length);
 
-    const suitElement = document.getElementById("suit");
-    const suitTwoElement = document.getElementById("suitTwo");
+    const suitsElement = document.getElementById("suits");
+    const suitsTwoElement = document.getElementById("suitsTwo");
 
-    suitElement.style.color = "black";
-    suitTwoElement.style.color = "black";
+    suitsElement.style.color = "black";
+    suitsTwoElement.style.color = "black";
 
-    suitElement.textContent = suite[suiteIndex];
-    suitTwoElement.textContent = suite[suiteIndex];
+    suitsElement.textContent = suits[suitsIndex];
+    suitsTwoElement.textContent = suits[suitsIndex];
 
-    const cardRankingElement = document.getElementById("cardRanking");
-    cardRankingElement.textContent = card[cardIndex];
+    const cardsRankingElement = document.getElementById("cardsRanking");
+    cardsRankingElement.textContent = cards[cardsIndex];
 
-    if (suite[suiteIndex] === "♥" || suite[suiteIndex] === "♦") {
-      suitElement.style.color = "red";
-      suitTwoElement.style.color = "red";
+    if (suits[suitsIndex] === "♥" || suits[suitsIndex] === "♦") {
+      suitsElement.style.color = "red";
+      suitsTwoElement.style.color = "red";
     }
     return console.log(
-      [suite[suiteIndex]],
-      [card[cardIndex]],
-      [suite[suiteIndex]]
+      [suits[suitsIndex]],
+      [cards[cardsIndex]],
+      [suits[suitsIndex]]
     );
   }
   generateCard();
   const generateButton = document.getElementById("generateButton");
   generateButton.addEventListener("click", generateCard);
+
+  let allCards = [];
+
+  for (let suit of suits) {
+    for (let card of cards) {
+      allCards.push(`${suit}${card}`);
+    }
+  }
+  console.log(allCards);
 };
 
-// 1. make 2 arrays one with card suites and one identifying cards Complete
+// 1. make 2 arrays one with card suits and one identifying cards Complete
 // 2. write a code function using math random to randomly pick a suit. 4 options. Complete
 // 2b. test and return results to console. Complete
 // 3. write a code function using math random to randomly pick a card. 14 options. Complete
@@ -61,4 +70,7 @@ window.onload = function() {
 // 5. transfer results to html using id. Complete
 // 6. upload suite icons Complete
 // 6b. Make the Heart and Diamond show up as red BEFORE printing Complete
-// 7. create button to continue randomizing more cards instead of refreshing page
+// 7. create button to continue randomizing more cards instead of refreshing page Completed
+// 8. Lets make another button that says Draw Poker
+// 9. Make hidden Div with 5 cards.
+// 10. using JS Gene
